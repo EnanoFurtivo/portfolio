a44231a3ae07de52154526172d0766ad08d2f6bd
Luciano Godoy <lucianogodoy@protonmail.com>
2024-09-11 15:02:20 -0300

    Fix React Router basename

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,31 @@
-import { ErrorPage, NotFoundPage, ProjectPage, RootPage } from "@/pages";
-import { createBrowserRouter } from "react-router-dom";
-import { MainLayout } from "@/layout";
+import { ErrorPage, NotFoundPage, ProjectPage, RootPage } from '@/pages';
+import { createBrowserRouter } from 'react-router-dom';
+import { MainLayout } from '@/layout';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        {
+          path: '/',
+          element: <RootPage />,
+          errorElement: <ErrorPage />
+        },
+        {
+          path: 'projects/:projectSlug',
+          element: <ProjectPage />,
+          errorElement: <ErrorPage />
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />
+    }
+  ],
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <RootPage />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "projects/:projectSlug",
-        element: <ProjectPage />,
-        errorElement: <ErrorPage />,
-      },
-    ],
-  },
-
-  {
-    path: "*",
-    element: <NotFoundPage />,
-  },
-]);
+    basename: '/portfolio'
+  }
+);
